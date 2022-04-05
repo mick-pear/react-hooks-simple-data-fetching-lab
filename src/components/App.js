1ef45ac81d5084987ea5a2c31f09b9d01f40fb25
Matthew <matthew_mcaleer@hotmail.com>
2022-04-05 13:15:44 +1000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
+import { useEffect } from "react/cjs/react.development"
+import { useState } from "react/cjs/react.development"
+
 // create your App component here
+export default function App() {
+
+    const [image, setImage] = useState(false)
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response=>response.json())
+        .then(data=>setImage(data))
+    })
+
+    return (
+        <>
+        {image ? <img src={image.message} alt="A Random Dog" /> : <p>Loading...</p>}
+        </>
+    )
+}
